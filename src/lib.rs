bff159e8b150f7cf7fb1351823ffe8d3aa375e21
adevries17 <andrew@andrewdevries.net>
2022-08-06 13:29:43 -0500

    release v0.2

---
+++ src/lib.rs
@@ -0,0 +1,45 @@
+use std::io;
+
+
+pub fn get_player_input() -> u8 {
+    loop {
+        // initial instructions to player
+        println!("Input your guess of rock: 1, paper: 2 or scissors: 3");
+
+        // obtain player input
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        let input_integer: u8 = input
+            .trim()
+            .parse()
+            .expect("Failed to convert to integer");
+
+        // check if valid value
+        match input_integer {
+            1 => return 1,
+            2 => return 2,
+            3 => return 3,
+            _ => continue,
+        }
+    }
+}
+
+pub fn determine_winner(p: &u8, c: &u8) {
+    match (p,c) {
+        (1,1) => println!("Tie!"), // player rock, computer rock
+        (1,2) => println!("You lose!"), // player rock, computer paper
+        (1,3) => println!("You win!"), // player rock, computer scissors
+
+        (2,1) => println!("You win!"), // player paper, computer rock
+        (2,2) => println!("Tie!"), // player paper, computer paper
+        (2,3) => println!("You lose!"), //player paper, computer scissors
+
+        (3,1) => println!("You lose!"), // player scissors, computer rock
+        (3,2) => println!("You win!"), // player scissors, computer paper
+        (3,3) => println!("Tie!"), // player scissors, computer scissors
+
+        _ => panic!("Unable to determine winner")
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,25 @@
-use std::io;
 use rand::Rng;
+use rps_rs::*;
 
-fn determine_winner(p:&u8, c:&u8) {
-    match (p, c) {
-        (1,1) => println!("Tie"),
-        _ => panic!("Unable to determine winner")
-    }
-}
+
 fn main() {
-    // initial instructions to player
+
     println!("Welcome to Rust rock paper scissors!");
-    println!("Input your guess of rock: 1, paper: 2 or scissors: 3");
 
 
-    // get player input
-    let mut player_input = String::new();
-
-    io::stdin()
-        .read_line(&mut player_input)
-        .expect("Failed to read input");
-    let player_integer:u8 = player_input
-        .trim()
-        .parse()
-        .expect("Failed to read input");
+    // get player guess
+    let player_guess = get_player_input();
 
 
     // get computer guess
-    let computer_guess:u8 = rand::thread_rng().gen_range(1..3);
+    let computer_guess: u8 = rand::thread_rng().gen_range(1..=3);
+
 
     println!("Computer chose: {computer_guess}");
-    println!("You chose: {player_integer}");
+    println!("You chose: {player_guess}");
 
 
     // determine winner
-    determine_winner(&player_integer, &computer_guess);
+    determine_winner(&player_guess, &computer_guess);
 
 }
