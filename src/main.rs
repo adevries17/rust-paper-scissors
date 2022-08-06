dbb223578929308732ab87e126ff806d66c91423
adevries17 <andrew@andrewdevries.net>
2022-08-06 08:45:43 -0500

    begin logic

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,38 @@
 use std::io;
-use rps_rs::Guess;
+use rand::Rng;
 
+fn determine_winner(p:&u8, c:&u8) {
+    match (p, c) {
+        (1,1) => println!("Tie"),
+        _ => panic!("Unable to determine winner")
+    }
+}
 fn main() {
     // initial instructions to player
     println!("Welcome to Rust rock paper scissors!");
-    println!("Input your guess of (r)ock, (p)aper, or (s)cissors:");
-    println!("rock, paper, scissors");
+    println!("Input your guess of rock: 1, paper: 2 or scissors: 3");
+
+
     // get player input
-    let mut player_input:String = String::new();
+    let mut player_input = String::new();
+
     io::stdin()
         .read_line(&mut player_input)
         .expect("Failed to read input");
+    let player_integer:u8 = player_input
+        .trim()
+        .parse()
+        .expect("Failed to read input");
 
-    let player_string:String = player_input.trim().parse().expect("This is not a valid guess");
 
-    let c_guess: Guess = Guess::computer_guess();
-    let u_guess: Guess = Guess::player_guess(player_string);
+    // get computer guess
+    let computer_guess:u8 = rand::thread_rng().gen_range(1..3);
 
-    println!("Computer guess: {:?}", c_guess);
-    println!("player guess: {:?}", u_guess);
+    println!("Computer chose: {computer_guess}");
+    println!("You chose: {player_integer}");
+
+
+    // determine winner
+    determine_winner(&player_integer, &computer_guess);
+
 }
