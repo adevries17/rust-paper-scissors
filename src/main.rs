98df903c753b2bd26c314450593ebe10065ddc5f
adevries17 <andrew@andrewdevries.net>
2022-08-05 19:55:46 -0500

    move parts to lib

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::io;
-use rand::Rng;
+use rps_rs::Guess;
 
 fn main() {
     // initial instructions to player
     println!("Welcome to Rust rock paper scissors!");
-    println!("Input your choice of rock, paper, or scissors:");
+    println!("Input your guess of (r)ock, (p)aper, or (s)cissors:");
     println!("rock, paper, scissors");
     // get player input
     let mut player_input:String = String::new();
@@ -12,39 +12,11 @@ fn main() {
         .read_line(&mut player_input)
         .expect("Failed to read input");
 
-    let player_string:String = player_input.trim().parse().unwrap();
+    let player_string:String = player_input.trim().parse().expect("This is not a valid guess");
 
-    let c_choice: Choice = Choice::computer_choice();
-    let u_choice: Choice = Choice::player_choice(player_string);
+    let c_guess: Guess = Guess::computer_guess();
+    let u_guess: Guess = Guess::player_guess(player_string);
 
-    println!("Computer choice: {:?}", c_choice);
-    println!("player choice: {:?}", u_choice);
-}
-
-#[derive(Debug)]
-pub enum Choice {
-    Rock,
-    Paper,
-    Scissors,
-}
-impl Choice {
-    // function to get computer's choice
-    pub fn computer_choice() -> Choice {
-        let computer_int: usize = rand::thread_rng().gen_range(1..3);
-        match &computer_int {
-            1 => Choice::Rock,
-            2 => Choice::Paper,
-            3 => Choice::Scissors,
-            _ => panic!()
-        }
-    }
-    // function to convert the player input a choice
-    pub fn player_choice(u_choice: String) -> Choice {
-        match &*u_choice {
-            "rock" => Choice::Rock,
-            "paper" => Choice::Paper,
-            "scissors" => Choice::Scissors,
-            _ => panic!("Not a valid option. Input rock, paper or scissors")
-        }
-    }
+    println!("Computer guess: {:?}", c_guess);
+    println!("player guess: {:?}", u_guess);
 }
